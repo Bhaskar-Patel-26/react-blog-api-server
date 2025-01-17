6aa72f2d6cde5e5ac608f0a982fbbadb7f06caf1
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2025-01-17 12:15:33 +0530

    MongoDB connection is completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRouters from "./routes/userRoutes.js";
 import postRouters from "./routes/postRoutes.js";
 import commentRouters from "./routes/commentRoutes.js";
+import connectDB from "./lib/connectDB.js";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use("/posts", postRouters);
 app.use("/comment", commentRouters);
 
 app.listen(3000, () => {
+  connectDB();
   console.log("server is running...");
 });
