8140584313249db5dae6dad72dd9faf3c858cc02
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2025-01-19 18:45:00 +0530

    Post routes and controllers are completed

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ import commentRouters from "./routes/commentRoutes.js";
 import connectDB from "./lib/connectDB.js";
 
 const app = express();
+app.use(express.json());
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message || "Something went wrong!",
+    status: error.status,
+    Stack: error.stack,
+  });
+});
 
 app.use("/users", userRouters);
 app.use("/posts", postRouters);
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { getPosts, getPost, createPost, deletePost } from "../controllers/postControllers.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.status(200).send("Post route");
-});
+router.get("/", getPosts);
+router.get("/:slug", getPost);
+router.post("/", createPost);
+router.delete("/:id", deletePost);
 
 export default router;
